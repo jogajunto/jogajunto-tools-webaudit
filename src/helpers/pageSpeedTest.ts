728a6a47968a42a6f3ab2e4a83be862a219df0f6
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-12-08 10:32:01 -0300

    Refactored pageSpeedTest for improved documentation and fetch logic

--- src/helpers/pageSpeedTest.ts
+++ src/helpers/pageSpeedTest.ts
@@ -1,66 +1,75 @@
 import { addToExcel } from './addToExcel';
 
-// Função para testar o PageSpeed
+/**
+ * Performs a PageSpeed test on a given URL and logs the results.
+ * @param testUrl - The URL of the page to test.
+ * @param testStrategy - The strategy to use for the PageSpeed test (e.g., 'mobile', 'desktop').
+ * @param filePath - The file path where the Excel report will be saved.
+ * @async
+ */
 export async function pageSpeedTest(
-	testUrl: string,
-	testStrategy: string,
-	browser: string,
-	filePath: string
+  testUrl: string,
+  testStrategy: string,
+  filePath: string
 ) {
-	try {
-		const response = await fetch(
-			`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${testUrl}&strategy=${testStrategy}`
-		);
-		if (response.ok) {
-			const json = await response.json();
-			const pageSpeedScore =
-				json.lighthouseResult.categories.performance.score * 100;
+  try {
+    // Fetches the PageSpeed Insights data from the Google API.
+    const response = await fetch(
+      `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${testUrl}&strategy=${testStrategy}`
+    );
+    if (response.ok) {
+      // Processes the response data if the request was successful.
+      const json = await response.json();
+      const pageSpeedScore =
+        json.lighthouseResult.categories.performance.score * 100;
 
-			// Coleta apenas as métricas específicas necessárias
-			const metrics = json.lighthouseResult.audits;
-			const fcp = metrics['first-contentful-paint'].displayValue;
-			const lcp = metrics['largest-contentful-paint-element'].displayValue;
-			const tti = metrics['interactive'].displayValue;
-			const cls = metrics['cumulative-layout-shift'].displayValue;
-			const tbw = metrics['total-byte-weight'].displayValue;
-			const unusedJs = metrics['unused-javascript'].displayValue;
+      // Extracts specific metrics from the PageSpeed results.
+      const metrics = json.lighthouseResult.audits;
+      const fcp = metrics['first-contentful-paint'].displayValue;
+      const lcp = metrics['largest-contentful-paint-element'].displayValue;
+      const tti = metrics['interactive'].displayValue;
+      const cls = metrics['cumulative-layout-shift'].displayValue;
+      const tbw = metrics['total-byte-weight'].displayValue;
+      const unusedJs = metrics['unused-javascript'].displayValue;
 
-			await addToExcel(
-				[
-					testUrl,
-					testStrategy,
-					pageSpeedScore.toString(),
-					fcp,
-					lcp,
-					tti,
-					cls,
-					tbw,
-					unusedJs,
-				],
-				{
-					filePath: filePath,
-					sheetName: 'PageSpeed Metrics',
-					columns: [
-						'URL',
-						'Device',
-						'Score',
-						'FCP',
-						'LCP',
-						'TTI',
-						'CLS',
-						'Total Byte Weight',
-						'Unused JS',
-					],
-				}
-			);
-		} else {
-			console.error('HTTP-Error: ' + response.status);
-		}
-	} catch (error: unknown) {
-		if (error instanceof Error) {
-			console.error(error.message);
-		} else {
-			console.error("An unknown error occurred");
-		}
-	}
+      // Saves the extracted metrics to an Excel file.
+      await addToExcel(
+        [
+          testUrl,
+          testStrategy,
+          pageSpeedScore.toString(),
+          fcp,
+          lcp,
+          tti,
+          cls,
+          tbw,
+          unusedJs,
+        ],
+        {
+          filePath: filePath,
+          sheetName: 'PageSpeed Metrics',
+          columns: [
+            'URL',
+            'Device',
+            'Score',
+            'FCP',
+            'LCP',
+            'TTI',
+            'CLS',
+            'Total Byte Weight',
+            'Unused JS',
+          ],
+        }
+      );
+    } else {
+      // Logs an error message if the HTTP request failed.
+      console.error('HTTP-Error: ' + response.status);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
+  }
 }
