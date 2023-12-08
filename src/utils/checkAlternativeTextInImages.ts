b94bff54a62ed4efececc6a6fa6ce483bbca212f
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-12-08 10:53:11 -0300

    Optimized check for alternative text in images

--- src/utils/checkAlternativeTextInImages.ts
+++ src/utils/checkAlternativeTextInImages.ts
@@ -13,49 +13,48 @@ import { addToExcel } from '../helpers/addToExcel';
  * @returns {Promise<void>}
  */
 const checkAlternativeTextInImages = async (
-	page: Page,
-	url: string,
-	filePath: string,
-	sheetName: string,
-	sheetColumns: Array<string>
+  page: Page,
+  url: string,
+  filePath: string,
+  sheetName: string,
+  sheetColumns: Array<string>
 ): Promise<void> => {
-	try {
-		await page.goto(url);
+  try {
+    await page.goto(url);
 
-		// Verificar se todas as imagens têm texto alternativo
-		const imagesWithoutAlt = await page.$$eval('img', (images) =>
-			images.filter((img) => !img.alt).map((img) => img.outerHTML)
-		);
+    // Check if all images have alt text
+    const imagesWithoutAlt = await page.$$eval('img', (images) =>
+      images.filter((img) => !img.alt).map((img) => img.outerHTML)
+    );
+    if (imagesWithoutAlt.length > 0) {
+      // Using regular expression to extract value from src attribute
+      const imgSrcs = imagesWithoutAlt.map((imgTag) => {
+        // TODO - validate that it is really an image url before trying to get the src value
+        const match = imgTag.match(/src="(.*?)"/);
+        return match ? match[1] : null; // Returns the value of the src attribute if found
+      });
 
-		if (imagesWithoutAlt.length > 0) {
-			// Usando expressão regular para extrair o valor do atributo src
-			const imgSrcs = imagesWithoutAlt.map((imgTag) => {
-				// TODO - validar se é mesmo uma url de imagem antes de tentar pegar o valor de src
-				const match = imgTag.match(/src="(.*?)"/);
-				return match ? match[1] : null; // Retorna o valor do atributo src se encontrado
-			});
+      // Add the results to the Excel spreadsheet
+      await addToExcel([url, imgSrcs.join(', ')], {
+        filePath: filePath,
+        sheetName: sheetName,
+        columns: sheetColumns,
+      });
+    }
 
-			// Adicionar os resultados à planilha Excel
-			await addToExcel([url, imgSrcs.join(', ')], {
-				filePath: filePath,
-				sheetName: sheetName,
-				columns: sheetColumns,
-			});
-		}
-
-		expect(imagesWithoutAlt.length).toBe(0);
-	} catch (error: unknown) {
-		if (error instanceof Error) {
-			// Adicionar o erro à planilha Excel
-			await addToExcel([url, error.message], {
-				filePath: filePath,
-				sheetName: sheetName,
-				columns: sheetColumns,
-			});
-		} else {
-			console.error("An unknown error occurred");
-		}
-	}
+    expect(imagesWithoutAlt.length).toBe(0);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      // Add the error to the Excel spreadsheet
+      await addToExcel([url, error.message], {
+        filePath: filePath,
+        sheetName: sheetName,
+        columns: sheetColumns,
+      });
+    } else {
+      console.error('An unknown error occurred');
+    }
+  }
 };
 
 export default checkAlternativeTextInImages;
