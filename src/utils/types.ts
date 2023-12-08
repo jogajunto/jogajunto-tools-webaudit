5a6657f8fe6268ac41468e28be7399875f8f0be0
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-12-08 10:56:37 -0300

    Updated types.ts with MetaTags type definitions

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,7 @@
+import { ElementHandle } from '@playwright/test';
+
 export type language = 'pt-BR' | 'en' | 'es';
+
+export interface MetaTags {
+  [key: string]: ElementHandle<HTMLElement | SVGElement> | null;
+}
