235a5c462a9a9a7ec9fb9e23d6dde6edb008d2a4
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-12-13 21:44:04 -0300

    Improve checkValidLinks for More Effective Link Validation

--- src/utils/checkValidLinks.ts
+++ src/utils/checkValidLinks.ts
@@ -1,39 +1,31 @@
-import { Page, expect } from '@playwright/test';
-import { addToExcel } from '../helpers/addToExcel';
+import { Page, expect } from "@playwright/test";
+import { addToExcel } from "../helpers/addToExcel";
 
 /**
  * Checks and validates the links on a webpage.
  * @param page - The Playwright Page object representing the page to check.
  * @param url - The URL of the page where links will be checked.
  * @param filePath - The file path where the Excel report will be saved.
- * @param sheetName - The name of the sheet in the Excel file to store the data.
- * @param sheetColumns - An array representing the columns for the Excel sheet.
  * @async
  */
-const checkValidLinks = async (
-  page: Page,
-  url: string,
-  filePath: string,
-  sheetName: string,
-  sheetColumns: Array<string>
-) => {
-  await page.goto(url); // Navigates to the specified URL.
+const checkValidLinks = async (page: Page, url: string, filePath: string) => {
+  try {
+    await page.goto(url); // Navigates to the specified URL.
 
-  // Fetches all href attributes from anchor tags on the page.
-  const links = await page.$$eval('a', (as) => as.map((a) => a.href));
+    // Fetches all href attributes from anchor tags on the page.
+    const links = await page.$$eval("a", (as) => as.map((a) => a.href));
 
-  // Iterates over each link to check its validity.
-  for (let link of links) {
-    try {
+    // Iterates over each link to check its validity.
+    for (let link of links) {
       // Checks if the link is an email or a number
       const isEmail = /^mailto:.+@.+\..+$/.test(link);
       const isNumber = /^\d+$/.test(link);
 
       if (isEmail || isNumber) {
-        await addToExcel([url, link, `Pulando link não válido`], {
+        await addToExcel([url, link, `Skipping invalid link`], {
           filePath: filePath,
-          sheetName: sheetName,
-          columns: sheetColumns,
+          sheetName: "Url Check",
+          columns: ["URL", "Link", "Status Code"],
         });
         continue; // Skip to next link
       }
@@ -41,18 +33,19 @@ const checkValidLinks = async (
       if (response) {
         await addToExcel([url, link, response.status()], {
           filePath: filePath,
-          sheetName: sheetName,
-          columns: sheetColumns,
+          sheetName: "Url Check",
+          columns: ["URL", "Link", "Status Code"],
         });
         expect(response.status()).not.toBe(404);
       }
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(error.message);
-      } else {
-        console.error('An unknown error occurred');
-      }
     }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("An unknown error occurred");
+    }
+    throw error;
   }
 };
 
