e132cd27ef99989555a2696b65fadd8c71126748
rafaell1995 <rafaeldearaujop95@gmail.com>
2024-02-12 10:23:33 -0300

    Add new types from use in checkButtonLinkStates

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,21 @@
-import { ElementHandle } from '@playwright/test';
+import { ElementHandle } from "@playwright/test";
 
-export type language = 'pt-BR' | 'en' | 'es';
+export type language = "pt-BR" | "en" | "es";
 
 export interface MetaTags {
   [key: string]: ElementHandle<HTMLElement | SVGElement> | null;
 }
+
+// Type definitions for the style states of elements when interacted with.
+export type ElementStyleState = {
+  backgroundColor: string;
+  color: string;
+  outerHTML: string;
+};
+
+// Type definition for the interaction styles of elements, which includes hover, focus, and active states.
+export type ElementInteractionStyles = {
+  hover: Array<ElementStyleState | null>;
+  focus: Array<ElementStyleState | null>;
+  active: Array<ElementStyleState | null>;
+};
