a521be4905d15dca4935424ecc18c9ef0fe49aa2
rafaell1995 <rafaeldearaujop95@gmail.com>
2024-02-12 10:15:00 -0300

    Fix export utils in project index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { pageSpeedTest } from "./helpers/pageSpeedTest";
 import { toSlug } from "./helpers/toSlug";
 import checkAccessibilityViolations from "./utils/checkAccessibilityViolations";
 import checkAlternativeTextInImages from "./utils/checkAlternativeTextInImages";
-import checkHeaderTags from "./utils/checkHeaderTags";
+import checkHeadingTags from "./utils/checkHeadingTags";
 import checkMetaTags from "./utils/checkMetaTags";
 import checkOgTags from "./utils/checkOgTags";
 import checkSpellingErrors from "./utils/checkSpellingErrors";
@@ -14,6 +14,10 @@ import checkCanonicalTag from "./utils/checkCanonicalTag";
 import checkFontsWoff2 from "./utils/checkFontsWoff2";
 import checkTagManager from "./utils/checkTagManager";
 import checkImagesLazyWidthHeight from "./utils/checkImagesLazyWidthHeight";
+import checkFavIcons from "./utils/checkFavIcons";
+import checkButtonLinkStates from "./utils/checkButtonAndLinkStates";
+import checkNavigationButtons from "./utils/checkNavigationButtons";
+import checkNavigationKeyboardTab from "./utils/checkNavigationKeyboardTab";
 
 // Exporting functions to be accessible to anyone importing your library
 export {
@@ -27,9 +31,13 @@ export {
   checkFontsWoff2,
   checkTagManager,
   checkImagesLazyWidthHeight,
-  checkHeaderTags,
+  checkButtonLinkStates,
+  checkNavigationButtons,
+  checkNavigationKeyboardTab,
+  checkHeadingTags,
   checkMetaTags,
   checkOgTags,
   checkSpellingErrors,
   checkValidLinks,
+  checkFavIcons,
 };
